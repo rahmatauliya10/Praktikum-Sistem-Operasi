a435546f986fa8af132442628e0e9f58a7d01025
rahmatauliya10 <54567261+rahmatauliya10@users.noreply.github.com>
2019-12-05 23:35:22 +0700

    Update 2.sh

--- Legacy Codes/2.sh
+++ Legacy Codes/2.sh
@@ -1,75 +1,20 @@
-#! /bin/bash
-
-pilihan=0
-
-while [ $pilihan -ne 4 ]
+echo "Program FIZZBUZZ";
+echo -n "masukkan batasnya : ";
+read bil;
+x=1
+while [ $x -le $bil ]
 do
-    echo "== Program Nilai Terbesar Kedua di dalam Array =="
-    echo "1. Demo Program Utama"
-    echo "2. Lihat Data Array"
-    echo "3. Pemjumlahan Data Array"
-    echo "4. Exit"
-    echo "Pilihan : "
-
-    read pilihan
-
-    case $pilihan in
-        1)
-            echo "Masukkan banyak elemen array : "
-            read max
-            for ((angka=0; angka<$max; angka=angka+1))
-            do
-                printf "Index Array - $angka : "
-                read inputan
-                utang[$angka]=$inputan
-            done
-            max=0
-            max2=0
-            index=0
-            iter=0
-            for perulangan in ${utang[@]}
-            do
-                if [ $perulangan -gt $max ]
-                then
-                    max=$perulangan
-                fi
-                let iter=$iter+1
-            done
-            iter=0
-            for perulangan in ${utang[@]}
-            do
-                if [ $perulangan -lt $max ] && [ $perulangan -gt $max2 ]
-                then
-                    max2=$perulangan
-                    index=$iter
-                fi
-                let iter=$iter+1
-            done
-            printf "Nilai terbesar kedua di dalam array adalah : ${utang[index]}\n"
-            printf "Index array ke : $index\n"
-            ;;
-        2)
-            if [ "${#utang[@]}" -ne 0 ]
-            then
-                echo "${utang[@]}"
-            else
-                echo "Data array masih kosong"
-            fi
-            ;;
-        3)
-            if [ "${#utang[@]}" -ne 0 ]
-            then
-                for perulangan in ${utang[@]}
-                do
-                    let sum=sum+$perulangan
-                done
-                echo "Jumlah all data array : $sum"
-            else
-                echo "Data array masih kosong"
-            fi
-            ;;
-        4)
-            echo "Terima Kasih Telah Menggunakan Program Ini"
-            ;;
-    esac
+    if [[ 0 -eq "($x%3)+($x%5)" ]]
+    then
+    echo "FizzBuzz";
+    elif [[ 0 -eq "($x%5)" ]]
+    then
+    echo "buzz";
+    elif [[ 0 -eq "($x%3)" ]]
+    then
+    echo "Fizz"
+    else
+    echo "$x";
+    fi
+    x=$(($x+1))
 done
